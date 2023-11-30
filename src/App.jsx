0ed358c0d94b1af4d227cfd5fca4761dbb7bc06d
gingerwnshp <grmnche@yandex.ru>
2023-11-30 15:42:27 +0300

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home.jsx';
-import { LibAuthors } from './pages/LibAuthors.jsx';
-import { About } from './pages/About.jsx';
-import { BookAuthors } from './pages/BookAuthors.jsx';
-import { BookStoreChernyshev } from './pages/chernyshev/BookStoreChernyshev.jsx';
-import { LibChernyshev } from './pages/chernyshev/LibChernyshev.jsx';
-import { LibHolmov } from './pages/holmov/LibHolmov.jsx';
-import { BookStoreHolmov } from './pages/holmov/BookStoreHolmov.jsx';
-import Footer from './components/Footer.jsx';
-
+import { LibAuthors } from './pages/LibAuthors/LibAuthors.jsx';
+import { About } from './pages/About/About.jsx';
+import { BookAuthors } from './pages/BookAuthors/BookAuthors.jsx';
+import { BookStoreChernyshev } from './pages/BookAuthors/BookStore/BookStoreChernyshev.jsx';
+import { LibChernyshev } from './pages/LibAuthors/Lib/LibChernyshev.jsx';
+import { LibHolmov } from './pages/LibAuthors/Lib/LibHolmov.jsx';
+import { BookStoreHolmov } from './pages/BookAuthors/BookStore/BookStoreHolmov.jsx';
+import Footer from './components/Footer/Footer.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
 
 function App() {
-  
-
   return (
     <HashRouter>
-      <div className='App'>
+      <div className="App">
+        <Navbar />
         <Routes>
           <Route path={'/'} exact Component={Home} />
           <Route path={'/libAuthors'} Component={LibAuthors} />
           <Route path={'/booksAuthors'} Component={BookAuthors} />
           <Route path={'/about'} Component={About} />
 
-          <Route path={'/booksChernyshev'} exact Component={BookStoreChernyshev} />
+          <Route
+            path={'/booksChernyshev'}
+            exact
+            Component={BookStoreChernyshev}
+          />
           <Route path={'/booksHolmov'} exact Component={BookStoreHolmov} />
 
           <Route path={'/libChernyshev'} exact Component={LibChernyshev} />
           <Route path={'/libHolmov'} exact Component={LibHolmov} />
         </Routes>
 
-      <Footer />
-
+        <Footer />
       </div>
     </HashRouter>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,10 @@
-import React, {Fragment, useEffect} from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Greet from '../components/Greet.jsx';
 
-
-import Navbar from '../components/Navbar.jsx';
-
 export function Home() {
-
   return (
     <Fragment>
-      <Navbar />
-
       <Greet />
     </Fragment>
   );
-};
+}
